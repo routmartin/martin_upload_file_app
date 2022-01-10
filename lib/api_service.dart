4c924108dfa0e7d64747d9d815bac48316de8631
Oun Rout <53936002+Routos@users.noreply.github.com>
2022-01-10 15:01:56 +0700

    complete implementation uploading

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,52 +1,66 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const baseUrl = "https://dev-api.kofi.com.kh";
+const fullBaseUrl = baseUrl + "/client/";
+const presign = fullBaseUrl + "upload/presign";
+const _tokenKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywidHlwZSI6ImN1c3RvbWVyIiwiaWF0IjoxNjQxNzc5OTc0LCJleHAiOjE2NDE4NjYzNzR9.0cff0WPdAxrXuK0tYVTuyRzLttKbPc7dxEftCSFR5EY";
+Dio _dio = Dio()..interceptors.add(PrettyDioLogger());
 
 class PresignApi {
-  static const baseUrl = "https://dev-api.kofi.com.kh";
-  static const fullBaseUrl = baseUrl + "/client/";
-  static const presign = fullBaseUrl + "upload/presign";
-
-  Dio _dio = Dio()
-    ..interceptors.add(InterceptorsWrapper(
-      onRequest: (options, handler) {
-        options.headers.addAll({"authorization": "Bearer"});
-      },
-    ));
-
-  Future<PresignResponese> presignUploadApi(
-      String extension, String fileName, String type) async {
-    final Response response = await _dio.post(presign, data: {
-      "media": [
-        {"ext": extension, "type": type, "filename": fileName},
-      ]
-    });
+  presignUploadApi(List<Media> data) async {
+    Response? response = await _dio.post(presign,
+        options: Options(headers: {"authorization": "Bearer $_tokenKey"}),
+        data: jsonEncode({'media': data}));
     return PresignResponese.fromJson(response.data);
   }
 
-  Future uploadFile(uploadFile, uploadUrl) async {
-    List<int> imageBytes = File(uploadFile.path).readAsBytesSync();
-    var file = MultipartFile.fromBytes(imageBytes).finalize();
-    Response _response = await _dio.put(
-      uploadUrl,
-      data: file,
-      options: Options(
-        headers: {
-          'Content-Type': 'image/jpeg',
-          'Accept': "*/*",
-          'Connection': 'keep-alive',
+  Future<bool> uploadFile(XFile uploadFile, String? uploadUrl,
+      Function(int) updatePercentage, CancelToken token) async {
+    List<int> _imageBytes = File(uploadFile.path).readAsBytesSync();
+    // Stream<List<int>> _file = MultipartFile.fromBytes(_imageBytes).finalize();
+
+    String? _extension = extension(uploadFile.path).split('.').last;
+    String _contentType = _extension == 'png' ? "image/png" : "image/jpeg";
+
+    /// Process api request
+
+    try {
+      final Response _response = await _dio.put(
+        uploadUrl!,
+        cancelToken: token,
+        data: _imageBytes,
+        options: Options(
+          headers: {
+            'Content-Type': _contentType,
+            'Accept': "*/*",
+            'Connection': 'keep-alive',
+            'Content-Length': File(uploadFile.path).lengthSync().toString(),
+          },
+          responseType: ResponseType.bytes,
+        ),
+        onSendProgress: (count, total) {
+          if (total != -1) {
+            updatePercentage(
+                int.parse((count / total * 100).toStringAsFixed(0)));
+          }
         },
-      ),
-    );
-    if (_response.statusCode == 200) return _response;
+      );
+      return _response.statusCode == 200 ? true : false;
+    } catch (e) {
+      throw e;
+    }
   }
 }
 
-// ** Modal Data
-
 class PresignResponese {
   PresignResponese({this.data});
-
   final List<Presign>? data;
 
   factory PresignResponese.fromJson(Map<String, dynamic> json) =>
@@ -69,3 +83,15 @@ class Presign {
         accessUrl: json["accessUrl"],
       );
 }
+
+class Media {
+  final String ext;
+  final String type;
+  final String fileName;
+
+  Media(this.ext, this.type, this.fileName);
+  Map<String, dynamic> toJson() =>
+      {"ext": ext, "type": type, "filename": fileName};
+}
+
+PresignApi apiService = new PresignApi();
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
+import 'dart:ui';
 
+import 'package:dio/dio.dart';
 import 'package:dotted_border/dotted_border.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
 
 import 'api_service.dart';
 
@@ -21,30 +24,123 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController loadingController;
+enum UploadingState { init, selected, uploading, error, comepleted }
 
-  PresignApi _uploadApiService = PresignApi();
+class _HomePageState extends State<HomePage> {
+  CancelToken _token = CancelToken();
+  List<XFile>? _fileList = [];
+  List<Media>? _mediaModelList = [];
+  List<Presign>? _presignList = [];
+  List<int>? _uploadSuccessList = [];
+  UploadingState _uploadState = UploadingState.init;
+  int _uploadIndex = 0;
+  int _uploadPercentage = 0;
 
-  List<File>? _file = [];
-  List<String>? _filesName = [];
-
-  _selectFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      allowMultiple: true,
-      type: FileType.custom,
-      allowedExtensions: ['png', 'jpg', 'jpeg'],
-    );
-    if (result != null) {
-      _file = result.paths.map((String? path) => File(path!)).toList();
-      _filesName = result.names.map((e) => e).cast<String>().toList();
-    }
+  void _updatePercentage(int percentage) {
+    _uploadPercentage = percentage;
     setState(() {});
-    loadingController.forward();
   }
 
-  _uploadFunction() {}
+  void _clearLocalStore() {
+    _fileList?.clear();
+    _presignList?.clear();
+    _mediaModelList?.clear();
+    _uploadSuccessList?.clear();
+  }
+
+  void _selectFile() async {
+    _clearLocalStore();
+    _uploadIndex = 0;
+    _fileList = await ImagePicker().pickMultiImage();
+    if (_fileList != null) {
+      _uploadState = UploadingState.selected;
+      _fileList!.forEach((file) {
+        String? _extension = extension(file.path).split('.').last;
+        String? _fileName = file.path.split('/').last;
+        _mediaModelList!.add(Media(_extension, 'profile', _fileName));
+      });
+      setState(() {});
+    }
+  }
+
+  void _uploadFunction() async {
+    /// register path on server
+    _uploadState = UploadingState.uploading;
+    setState(() {});
+    try {
+      PresignResponese? _res =
+          await apiService.presignUploadApi(_mediaModelList!);
+      if (_res != null) {
+        _presignList = _res.data;
+        _putfileToServer();
+      } else {
+        _uploadState = UploadingState.error;
+        setState(() {});
+      }
+    } catch (e) {
+      _uploadState = UploadingState.error;
+      setState(() {});
+    }
+  }
+
+  void _putfileToServer() async {
+    if (_uploadState == UploadingState.uploading) {
+      _token = new CancelToken();
+    }
+    if (_uploadIndex < _fileList!.length) {
+      try {
+        bool _apiResponse = await apiService.uploadFile(
+            _fileList![_uploadIndex],
+            _presignList![_uploadIndex].uploadUrl,
+            _updatePercentage,
+            _token);
+        if (_apiResponse) {
+          _uploadSuccessList?.add(_uploadIndex);
+          setState(() {});
+          _uploadIndex += 1;
+          _uploadPercentage = 0;
+          _putfileToServer();
+        } else {
+          _uploadState = UploadingState.error;
+          setState(() {});
+        }
+      } catch (e) {
+        print('handle upload error here: $e');
+        _uploadState = UploadingState.error;
+        setState(() {});
+      }
+    } else {
+      _uploadState = UploadingState.comepleted;
+      setState(() {});
+    }
+  }
+
+  void _retryUploading() {
+    _uploadState = UploadingState.uploading;
+    _putfileToServer();
+  }
+
+  void _removeUploading(int index) {
+    _fileList!.removeAt(index);
+    setState(() {});
+  }
+
+  void _pauseUploading() {
+    _token.cancel();
+  }
+
+  void _actionHandler() {
+    switch (_uploadState) {
+      case UploadingState.init:
+        return print('please select the files');
+      case UploadingState.selected:
+        return _uploadFunction();
+      case UploadingState.comepleted:
+        return _uploadFunction();
+      default:
+        return _retryUploading();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,18 +169,32 @@ class _HomePageState extends State<HomePage>
                         child: Container(
                           width: MediaQuery.of(context).size.width * .6,
                           height: 200,
-                          child: Icon(
-                            Iconsax.folder_open,
-                            color: Colors.blue,
-                            size: 40,
-                          ),
+                          child: _uploadState == UploadingState.init
+                              ? Icon(
+                                  Iconsax.folder_open,
+                                  color: Colors.blue,
+                                  size: 30,
+                                )
+                              : Center(
+                                  child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      " ${_uploadSuccessList!.length} / ${_fileList!.length}",
+                                      style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.blue.shade800,
+                                      ),
+                                    ),
+                                  ],
+                                )),
                         ),
                       ),
                     ),
                     SizedBox(width: 12),
                     Expanded(
                       child: InkWell(
-                        onTap: _selectFile,
+                        onTap: _actionHandler,
                         child: DottedBorder(
                           borderType: BorderType.RRect,
                           radius: Radius.circular(10),
@@ -94,11 +204,7 @@ class _HomePageState extends State<HomePage>
                           child: Container(
                             alignment: Alignment.center,
                             height: 200,
-                            child: Icon(
-                              Iconsax.document_upload,
-                              color: Colors.blue,
-                              size: 40,
-                            ),
+                            child: _iconBuilder(),
                           ),
                         ),
                       ),
@@ -108,10 +214,64 @@ class _HomePageState extends State<HomePage>
               ),
               Expanded(
                 child: ListView.builder(
-                  itemCount: _file?.length,
-                  itemBuilder: (ctx, index) =>
-                      _buildUploadCard(_file?[index], _filesName?[index]),
-                ),
+                    padding: EdgeInsets.all(12),
+                    itemCount: _fileList?.length,
+                    addAutomaticKeepAlives: true,
+                    cacheExtent: 10,
+                    itemBuilder: (ctx, index) {
+                      XFile? image = _fileList?[index];
+                      return Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.shade200,
+                                offset: Offset(0, 1),
+                                blurRadius: 3,
+                                spreadRadius: 2,
+                              )
+                            ]),
+                        child: Row(
+                          children: [
+                            Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(8),
+                                  child: Image.file(
+                                    File(image!.path),
+                                    width: 150,
+                                    height: 100,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                _onImageIconBuilder(index),
+                              ],
+                            ),
+                            SizedBox(width: 10),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    image.path.split('/').last,
+                                    style: TextStyle(
+                                        fontSize: 13, color: Colors.black),
+                                  ),
+                                  SizedBox(height: 15),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 10),
+                          ],
+                        ),
+                      );
+                    }),
               ),
             ],
           ),
@@ -119,57 +279,85 @@ class _HomePageState extends State<HomePage>
       ),
     );
   }
-}
 
-_buildUploadCard(File? image, String? name) {
-  return Container(
-      margin: EdgeInsets.only(bottom: 10),
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.shade200,
-              offset: Offset(0, 1),
-              blurRadius: 3,
-              spreadRadius: 2,
-            )
-          ]),
-      child: Row(
-        children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.file(
-                image!,
-                width: 150,
-                height: 100,
-                fit: BoxFit.cover,
-              )),
-          SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name.toString(),
-                  style: TextStyle(fontSize: 13, color: Colors.black),
-                ),
-                SizedBox(height: 5),
-                // Container(
-                //     height: 5,
-                //     clipBehavior: Clip.hardEdge,
-                //     decoration: BoxDecoration(
-                //       borderRadius: BorderRadius.circular(5),
-                //       color: Colors.blue.shade50,
-                //     ),
-                //     child: LinearProgressIndicator(
-                //       value: loadingController.value,
-                //     )),
-              ],
+  Widget _onImageIconBuilder(int index) {
+    Widget? _returnWidget;
+    if (_uploadSuccessList!.contains(index)) {
+      _returnWidget = Container(
+        padding: EdgeInsets.all(6),
+        decoration: BoxDecoration(
+            color: Colors.black.withOpacity(.7),
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            border: Border.all(color: Colors.grey.shade700)),
+        child: Icon(Icons.done, color: Colors.white, size: 30),
+      );
+    } else if (_uploadState == UploadingState.uploading &&
+        _uploadIndex == index) {
+      _returnWidget = InkWell(
+        onTap: _pauseUploading,
+        child: Container(
+          width: 50,
+          height: 50,
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade700),
+            color: Colors.black.withOpacity(.7),
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
+          child: Text(
+            '$_uploadPercentage',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
             ),
           ),
-          SizedBox(width: 10),
-        ],
-      ));
+        ),
+      );
+    } else if (_uploadState == UploadingState.error && _uploadIndex == index) {
+      _returnWidget = InkWell(
+        onTap: _retryUploading,
+        child: _returnWidget = Container(
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+              color: Colors.black.withOpacity(.7),
+              borderRadius: BorderRadius.all(Radius.circular(40)),
+              border: Border.all(color: Colors.grey.shade700)),
+          child: Icon(Icons.upload_sharp, color: Colors.white, size: 30),
+        ),
+      );
+    } else {
+      _returnWidget = InkWell(
+        onTap: () => _removeUploading(index),
+        child: Container(
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(.7),
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+          ),
+          child: Icon(Icons.close, color: Colors.white, size: 30),
+        ),
+      );
+    }
+    return _returnWidget;
+  }
+
+  Widget _iconBuilder() {
+    switch (_uploadState) {
+      case UploadingState.uploading:
+        return CircularProgressIndicator(strokeWidth: .8);
+      case UploadingState.error:
+        return Icon(
+          Iconsax.refresh_right_square,
+          color: Colors.red,
+          size: 30,
+        );
+      default:
+        return Icon(
+          Iconsax.document_upload,
+          color: Colors.blue,
+          size: 30,
+        );
+    }
+  }
 }
